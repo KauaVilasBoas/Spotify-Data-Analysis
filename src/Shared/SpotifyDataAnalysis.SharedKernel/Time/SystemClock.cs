92c4e2a70c26e349973a4211c14b344061d26e78
Kauã Vilas Boas <kauacaldeira@hotmail.com>
2026-08-17 00:01:51 -0300

    revert(e6.1): remover o warning intencional apos confirmar o CI vermelho (run 31989540301)

--- src/Shared/SpotifyDataAnalysis.SharedKernel/Time/SystemClock.cs
+++ src/Shared/SpotifyDataAnalysis.SharedKernel/Time/SystemClock.cs
@@ -1,5 +1,3 @@
-#warning E6.1 verificacao intencional: este warning deve deixar o CI VERMELHO e sera revertido
-
 namespace SpotifyDataAnalysis.SharedKernel.Time;
 
 /// <summary>Production implementation of <see cref="IClock"/>. Register as Singleton.</summary>
